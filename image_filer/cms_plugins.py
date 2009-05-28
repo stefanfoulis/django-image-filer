9ccf52c0b5b3279dd9d3b19382c5a220596c0a1d
Stefan Foulis <stefan.foulis@spiderware.ch>
2009-05-28 13:32:25 +0200

    cleaned up cms plugin a bit. thumbnail in text editor plugin works with current cms2

--- image_filer/cms_plugins.py
+++ image_filer/cms_plugins.py
@@ -3,22 +3,17 @@ from cms.plugin_base import CMSPluginBase
 from django.utils.translation import ugettext_lazy as _
 from image_filer.models import ImagePublication
 from cms.settings import CMS_MEDIA_URL
-#from image_filer.forms import ImagePublicationForm
 
 class ImagePlugin(CMSPluginBase):
     model = ImagePublication
     name = _("Image from Image Filer")
     render_template = "image_filer/image.html"
     text_enabled = True
-    #form = ImagePublicationForm
     change_form_template = 'admin/image_filer/cms/image_plugin/change_form.html'
     raw_id_fields = ('image',)
     
     def render(self, context, instance, placeholder):
         return {'image_publication':instance, 'placeholder':placeholder}
-    
     def icon_src(self, instance):
-        # TODO - possibly use 'instance' and provide a thumbnail image
-        return instance.image.admin_thumbnail()
- 
+        return instance.image.get_admin_tiny_thumbnail_url()
 plugin_pool.register_plugin(ImagePlugin)
